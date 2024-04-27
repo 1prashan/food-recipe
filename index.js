1e93357b41f8b6b172c0fedb7dbe31f8726ee0f2
neha-rajput <vs06237@gmail.com>
2024-04-28 03:48:38 +0530

    done

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ let name=prompt("What is your name dear");
 console.log(name);
  
 console.log("This is branch concept in git");
+
+const student={
+    name:'vishal singh',
+    rollno:223423,
+    section:'g2',
+    contry:'India',
+}
+for(let value in student){
+    console.log(value);
+}
